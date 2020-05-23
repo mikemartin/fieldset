96bfd209fb7f5b294e545950a46fbfc8f20851b8
Mike Martin <hello@mike-martin.ca>
2020-05-23 21:32:10 +1000

    Configure custom forms

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,10 +14,37 @@ module.exports = {
     },
     extend: {
       fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
         display: ['Nexa', ...defaultTheme.fontFamily.sans],
       },
-    }
+    },
+    customForms: theme => ({
+      default: {
+        'input, textarea, multiselect, select, checkbox, radio': {
+          borderColor: theme('colors.pink'),
+          backgroundColor: theme('colors.transparent')
+        },
+        'select, checkbox, radio': {
+          iconColor: theme('colors.pink'),
+        },
+        'input, textarea, multiselect, select': {
+          fontSize: theme('fontSize.sm'),
+          borderRadius: theme('borderRadius.md'),
+          padding: `${theme('spacing.3')} ${theme('spacing.5')}`,
+          letterSpacing: theme('letterSpacing.wide'),
+          color: theme('colors.white'),
+          '&::placeholder': {
+            color: theme('colors.pink'),
+          },
+          '&:focus': {
+            boxShadow: defaultTheme.boxShadow.none,
+            borderColor: theme('colors.pink'),
+            '&::placeholder': {
+              color: theme('colors.white')
+            },
+          }
+        },
+      },
+    })
   },
   variants: {
     textColor: ['responsive', 'hover', 'focus', 'active', 'group-hover'],
